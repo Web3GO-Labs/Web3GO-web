7e9eaa328df925fe407962f1005ba643801b9ca6
hailiting <hailiting@yeah.net>
2022-06-16 14:11:28 +0800

    add

--- src/utils/web3React.ts
+++ src/utils/web3React.ts
@@ -8,8 +8,14 @@ import { chainId } from 'config/constants/tokens';
 
 const POLLING_INTERVAL = 12000;
 const rpcUrl = getNodeUrl();
-
-const injected = new InjectedConnector({ supportedChainIds: [chainId] });
+// supportedChainIds
+const supportedChainIds = [
+  1, 3, 4, 5, 42, 52, 56, 81, 82, 87, 97, 100, 137, 226, 336, 592, 1337, 1887, 31337, 71393, 80001, 1666700000,
+  1666600000, 1666600001, 1666600002, 1666600003,
+];
+const injected = new InjectedConnector({
+  supportedChainIds: supportedChainIds,
+});
 
 const walletconnect = new WalletConnectConnector({
   rpc: { [chainId]: rpcUrl },
@@ -18,7 +24,7 @@ const walletconnect = new WalletConnectConnector({
   // pollingInterval: POLLING_INTERVAL,
 });
 
-const bscConnector = new BscConnector({ supportedChainIds: [chainId] });
+const bscConnector = new BscConnector({ supportedChainIds: supportedChainIds });
 
 export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
   [ConnectorNames.Injected]: injected,
